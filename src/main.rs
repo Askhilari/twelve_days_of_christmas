7f00ea86f381d8157e2b59c61488c386c48a0dc5
Askhilari <askhilari433@gmail.com>
2025-12-26 11:25:04 -0600

    Included more stars patterns. The code will be updated and made more user friendly so that user can select the output. Also,, patterns will be included

--- src/main.rs
+++ src/main.rs
@@ -20,4 +20,68 @@ fn main(){
         }
         println!("");
     }
+
+    println!("Simple triangle pattern");
+    triangle(5);
+    println!("Reverse triangle pattern");
+    reverse_triangle(5);
+    println!("Triangle pattern with only odd starts in rows");
+    odd_stars(5);
+    println!("Diamond Patterns");
+    diamond_pattern(5);
+    
 }
+
+
+    // Following code prints * in a triangle 1 - 5 format.
+fn triangle(n: usize) {
+    for i in 1..n+2 {
+        for _j in 1..i {
+            print!("*");
+        }
+        println!("");
+    }
+}
+
+    // Following code prints the above pattern in reverse
+fn reverse_triangle(n: usize) {    
+    for i in (1..n+2).rev(){
+        for _j in 1..i{
+            print!("*");
+        }
+
+        println!("");
+    }
+}
+
+// The following code prints odd number of stars in each line
+fn odd_stars(n: usize) {
+    for i in 0..n {
+        for _j in 0..(2*i + 1) {
+            print!("*");
+        }
+        println!("");
+    }
+}
+
+fn diamond_pattern(n: usize){
+    let mut space = n;
+    for i in 1..n{
+        print!("{}", " ".repeat(space));
+        for _j in 1..(2*i){
+            print!("*");
+        }
+        space -= 1;
+        println!(" ");
+    }
+
+    for i in (1..=n).rev(){
+        print!("{}", " ".repeat(space));
+        for _j in 1..(2*i){
+            print!("*");
+        }
+        space += 1;
+        println!(" ");
+    }
+}
+
